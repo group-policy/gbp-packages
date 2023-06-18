7043656d9d5e97561f52515d808395f85e24d552
irathore <rathore@noironetworks.com>
2023-06-18 14:30:34 -0700

    Removed all python2 stuff

--- group-based-policy-automation/rpm/build-rpm.sh
+++ group-based-policy-automation/rpm/build-rpm.sh
@@ -10,34 +10,6 @@ if [ -z $SPEC_FILE_IN ]; then
    echo "RPM spec file not found"
    exit 1
 fi
-BUILD_DIR=${BUILD_DIR:-`pwd`/rpmbuild}
-mkdir -p $BUILD_DIR/BUILD $BUILD_DIR/SOURCES $BUILD_DIR/SPECS $BUILD_DIR/RPMS $BUILD_DIR/SRPMS
-NAME=`python2 setup.py --name`
-RELEASE=${RELEASE:-1}
-VERSION_PY=`python2 setup.py --version`
-VERSION=`echo $VERSION_PY | sed -nre 's,([^\.]+.[^\.]+.[^\.]+)((\.)(0[^\.]+))?((\.)(dev.*))?,\1 \4 \7,p' | sed -re 's/ *$//g' | sed -re 's/ +/~/g'`
-SPEC_FILE=${SPEC_FILE_IN/.in/}
-SPEC_FILE=${SPEC_FILE/rpm\//}
-sed -e "s/@VERSION@/$VERSION/" \
-    -e "s/@VERSION_PY@/$VERSION_PY/" \
-    -e "s/@RELEASE@/$RELEASE/" \
-    $SPEC_FILE_IN > $BUILD_DIR/SPECS/$SPEC_FILE
-python2 setup.py sdist --dist-dir $BUILD_DIR/SOURCES
-mv $BUILD_DIR/SOURCES/$NAME-$VERSION_PY.tar.gz $BUILD_DIR/SOURCES/$NAME-$VERSION.tar.gz
-cp rpm/*.patch $BUILD_DIR/SOURCES
-rpmbuild --clean -ba --define "_topdir $BUILD_DIR" $BUILD_DIR/SPECS/$SPEC_FILE
-
-# Save the python2 packages
-cp rpmbuild/RPMS/noarch/*.rpm .
-cp rpmbuild/SRPMS/*.rpm .
-rm -rf rpmbuild
-
-# Prepare build scripts for python3
-cp $SPEC_FILE_IN .
-
-sed -i "s/python-/python3-/g" $SPEC_FILE_IN
-sed -i "s/python2/python3/g" $SPEC_FILE_IN
-sed -i "s/Name:           %{srcname}/Name:           python3-%{srcname}/g" $SPEC_FILE_IN
 
 BUILD_DIR=${BUILD_DIR:-`pwd`/rpmbuild}
 mkdir -p $BUILD_DIR/BUILD $BUILD_DIR/SOURCES $BUILD_DIR/SPECS $BUILD_DIR/RPMS $BUILD_DIR/SRPMS
